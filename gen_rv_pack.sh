9a35cd12c70e3eb27cb3d1e84dd73f5fafc476e7
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-29 21:15:45 +0800

    add script to generate rv_pack

---
+++ gen_rv_pack.sh
@@ -0,0 +1,22 @@
+SPLASH2DIR=$(pwd)
+
+ALL="barnes cholesky fft fmm lu_cb lu_ncb ocean_cp ocean_ncp radiosity radix raytrace volrend water_nsquared water_spatial"
+
+if [ ! -d "${SPLASH2DIR}/splash2_rv_pack" ]; then
+    mkdir -p ${SPLASH2DIR}/splash2_rv_pack
+else
+    rm -rf ${SPLASH2DIR}/splash2_rv_pack/*
+fi
+
+for PROGRAM in $ALL; do
+    mkdir -p ${SPLASH2DIR}/splash2_rv_pack/${PROGRAM}
+
+    mkdir -p ${SPLASH2DIR}/splash2_rv_pack/${PROGRAM}/build/rv64
+    cp -r ${SPLASH2DIR}/${PROGRAM}/build/rv64/bin ${SPLASH2DIR}/splash2_rv_pack/${PROGRAM}/build/rv64/bin
+    if [ -d "${SPLASH2DIR}/${PROGRAM}/inputs" ]; then
+        cp -r ${SPLASH2DIR}/${PROGRAM}/inputs ${SPLASH2DIR}/splash2_rv_pack/${PROGRAM}/inputs
+    fi
+    cp -r ${SPLASH2DIR}/${PROGRAM}/run.sh ${SPLASH2DIR}/splash2_rv_pack/${PROGRAM}/
+done
+
+cp -r ${SPLASH2DIR}/run.sh ${SPLASH2DIR}/splash2_rv_pack/
