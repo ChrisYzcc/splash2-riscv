efac5024d461dbe808f1c8605ff79668a1424672
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-29 19:10:52 +0800

    add run.sh for ocean_ncp

---
+++ ocean_ncp/run.sh
@@ -0,0 +1,13 @@
+BENCH_DIR="$(pwd)"
+RUN_DIR=${BENCH_DIR}/build/${PLATFORM}/run
+
+if [ ! -d "${RUN_DIR}" ]; then
+    mkdir -p "${RUN_DIR}"
+else
+    rm -rf "${RUN_DIR}"/*
+fi
+
+echo "Running ocean_cp..."
+cd ${RUN_DIR}
+${BENCH_DIR}/build/${PLATFORM}/bin/ocean_ncp -n258 -p${NTHREADS} -e1e-07 -r20000 -t28800
+cd ${BENCH_DIR}
