41694179b73e0245e6b355fa13b5616fd4f5949a
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-29 18:33:20 +0800

    add build for barnes

---
+++ build.sh
@@ -0,0 +1,97 @@
+export SPLASH2DIR=$(pwd)
+
+PLATFORM=native
+PROGRAM=barnes
+
+ALL=""
+
+while getopts "p:rh" opt; do
+    case "$opt" in
+        p) PROGRAM=$OPTARG ;;
+        r) PLATFORM=rv64 ;;
+        h) echo "Usage: $0 [-p program] [-r] [-h]"
+           echo "  -p program : specify the program to build, default: blackscholes"
+           echo "  -r          : set platform to rv64"
+           echo "  -h          : display this help message"
+           exit 0 ;;
+    esac
+done
+
+# Arguments to use
+export CFLAGS=" -O3 -g -funroll-loops ${PORTABILITY_FLAGS} -static"
+export CXXFLAGS="-O3 -g -funroll-loops -fpermissive -fno-exceptions ${PORTABILITY_FLAGS} -std=c++98 -static"
+export CPPFLAGS=""
+export CXXCPPFLAGS=""
+export LDFLAGS="-L${CC_HOME}/lib64 -L${CC_HOME}/lib -no-pie -static"
+export LIBS=""
+export EXTRA_LIBS=""
+
+# Compilers and preprocessors
+CC_HOME=/usr/bin
+export CC="${CC_HOME}/${CROSS_COMPILE_PREFIX}gcc"
+export CXX="${CC_HOME}/${CROSS_COMPILE_PREFIX}g++"
+export CPP="${CC_HOME}/${CROSS_COMPILE_PREFIX}cpp"
+
+# GNU Binutils
+BINUTIL_HOME=/usr/bin
+export LD="${BINUTIL_HOME}/${CROSS_COMPILE_PREFIX}ld"
+export NM="${BINUTIL_HOME}/${CROSS_COMPILE_PREFIX}nm"
+export STRIP="${BINUTIL_HOME}/${CROSS_COMPILE_PREFIX}strip"
+export AR="${CC_HOME}/${CROSS_COMPILE_PREFIX}ar"
+export RANLIB="${CC_HOME}/${CROSS_COMPILE_PREFIX}ranlib"
+
+# GNU Tools
+export M4=/usr/bin/m4
+export MAKE=/usr/bin/make
+
+export PLATFORM
+export VERSION
+
+if [ "${PROGRAM}" = "all" ]; then
+
+
+    FAILED_LIST=""
+    for prog in $ALL; do
+        echo "============================================================================"
+        echo "  Building Target : ${prog}"
+        echo "  Platform        : ${PLATFORM}"
+        echo "  Version         : ${VERSION}"
+        echo "============================================================================"
+        cd ${prog}
+        ./build.sh
+        if [ $? -ne 0 ]; then
+            echo -e "\033[31m[ERROR] Build failed for ${prog}!\033[0m"
+            FAILED_LIST="$FAILED_LIST $prog"
+        fi
+        cd ..
+    done
+
+    echo "============================================================================"
+    echo "  Build of all programs completed."
+    echo "============================================================================"
+    if [ -n "$FAILED_LIST" ]; then
+        echo -e "\033[31m[ERROR] The following programs failed to build:$FAILED_LIST\033[0m"
+        exit 1
+    fi
+    exit 0
+else
+    echo "============================================================================"
+    echo "  Building Target : ${PROGRAM}"
+    echo "  Platform        : ${PLATFORM}"
+    echo "  Version         : ${VERSION}"
+    echo "============================================================================"
+
+
+    cd ${PROGRAM}
+    ./build.sh
+    if [ $? -ne 0 ]; then
+        echo -e "\033[31m[ERROR] Build failed for ${PROGRAM}!\033[0m"
+        cd ..
+        exit 1
+    fi
+    cd ..
+
+    echo "============================================================================"
+    echo "  Build of ${PROGRAM} completed."
+    echo "============================================================================"
+fi
